e47a096e0586fddda9acbb99133a5e63ec3d9940
prudhvireddymali <prudhvireddymali2021@gmail.com>
2022-10-07 00:37:01 -0500

    reversing the string

---
+++ reverse.java
@@ -0,0 +1,17 @@
+package programming;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		String name="PRUDHVIREDDYMALI";
+		char[] str=name.toCharArray();
+		for(int i=str.length-1; i>=0; i--)
+		{
+			System.out.println(str[i]);
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
